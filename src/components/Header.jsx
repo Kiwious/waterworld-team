187886098e572e6c01b92169596f103ca7286b66
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2022-12-28 12:28:39 +0100

    Mogus

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Waterworld Team</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, fugiat.</p>
+        </header>
+    );
+};
+
+export default Header;  
